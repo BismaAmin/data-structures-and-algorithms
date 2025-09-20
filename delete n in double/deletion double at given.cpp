714a659095beedd005726993d15219f48a6e96e8
BismaAmin <aminbisma26@gmail.com>
2025-09-20 13:36:40 +0500

    Add files via upload

---
+++ delete n in double/deletion double at given.cpp
@@ -0,0 +1,97 @@
+#include<iostream>
+using namespace std;
+
+struct Node {
+    int data;
+    Node *next;
+    Node *prev;
+};
+
+// Insert at end
+void insertAtEnd(Node *&head, int value) {
+    Node *newnode = new Node();
+    newnode->data = value;
+    newnode->next = NULL;
+    newnode->prev = NULL;
+
+    if (head == NULL) {
+        head = newnode;
+        return;
+    }
+
+    Node *temp = head;
+    while (temp->next != NULL) {
+        temp = temp->next;
+    }
+    temp->next = newnode;
+    newnode->prev = temp;
+}
+
+// Delete at given position
+void deleteAtGiven(Node *&head, int pos) {
+    if (head == NULL) {
+        cout << "The list is empty, nothing to delete" << endl;
+        return;
+    }
+
+    if (pos == 1) { // delete first node
+        Node *temp = head;
+        head = head->next;
+        if (head != NULL) {
+            head->prev = NULL;
+        }
+        cout << "Deletion is done of " << temp->data << endl;
+        delete temp;
+        return;
+    }
+
+    Node *temp = head;
+    for (int i = 1; temp != NULL && i < pos; i++) {
+        temp = temp->next;
+    }
+
+    if (temp == NULL) {
+        cout << "Invalid position" << endl;
+        return;
+    }
+
+    // unlink node
+    if (temp->prev != NULL) {
+        temp->prev->next = temp->next;
+    }
+    if (temp->next != NULL) {
+        temp->next->prev = temp->prev;
+    }
+
+    cout << "Deletion is done of " << temp->data << endl;
+    delete temp;
+}
+
+// Display the list
+void display(Node *head) {
+    Node *temp = head;
+    while (temp != NULL) {
+        cout << "The elements are = " << temp->data << endl;
+        temp = temp->next;
+    }
+}
+
+int main() {
+    Node *head = NULL;
+
+    insertAtEnd(head, 10);
+    insertAtEnd(head, 20);
+    insertAtEnd(head, 30);
+    insertAtEnd(head, 40);
+    insertAtEnd(head, 50);
+
+    cout << "Before deletion:" << endl;
+    display(head);
+
+    deleteAtGiven(head, 3); // delete 3rd node (30)
+
+    cout << "After deletion at given position:" << endl;
+    display(head);
+
+    return 0;
+}
---
+++ delete n in double/deletion double end.cpp
@@ -0,0 +1,81 @@
+#include<iostream>
+using namespace std;
+
+struct Node {
+    int data;
+    Node *next;
+    Node *prev;
+};
+
+// Insert at end
+void insertAtEnd(Node *&head, int value) {
+    Node *newnode = new Node();
+    newnode->data = value;
+    newnode->next = NULL;
+    newnode->prev = NULL;
+
+    if (head == NULL) {
+        head = newnode;
+        return;
+    }
+
+    Node *temp = head;
+    while (temp->next != NULL) {
+        temp = temp->next;
+    }
+    temp->next = newnode;
+    newnode->prev = temp;
+}
+
+// Delete from end
+void deleteAtEnd(Node *&head) {
+    if (head == NULL) {
+        cout << "The list is empty, nothing to delete" << endl;
+        return;
+    }
+
+    // If only one node
+    if (head->next == NULL) {
+        cout << "Deletion is done of " << head->data << endl;
+        delete head;
+        head = NULL;
+        return;
+    }
+
+    Node *temp = head;
+    while (temp->next != NULL) {
+        temp = temp->next;
+    }
+
+    cout << "Deletion is done of " << temp->data << endl;
+    temp->prev->next = NULL;
+    delete temp;
+}
+
+// Display the list
+void display(Node *head) {
+    Node *temp = head;
+    while (temp != NULL) {
+        cout << "The elements are = " << temp->data << endl;
+        temp = temp->next;
+    }
+}
+
+int main() {
+    Node *head = NULL;
+
+    insertAtEnd(head, 10);
+    insertAtEnd(head, 20);
+    insertAtEnd(head, 30);
+    insertAtEnd(head, 40);
+
+    cout << "Before deletion:" << endl;
+    display(head);
+
+    deleteAtEnd(head);
+
+    cout << "After deletion from end:" << endl;
+    display(head);
+
+    return 0;
+}
